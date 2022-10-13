4dbf874bed818bd593f4f2edec662dbadb17a0c4
Julio Zambom <work.juliozambom@gmail.com>
2022-10-13 14:39:27 -0300

    adding navbar menu behavior

---
+++ js/navbar.js
@@ -0,0 +1,27 @@
+class NavBar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.navList.classList.toggle(this.activeClass);
+  }
+
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
+
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
+    }
+    return this;
+  }
+}
+
+const navbarBehavior = new NavBar("nav", "nav ul", "nav ul li");
+navbarBehavior.init();
